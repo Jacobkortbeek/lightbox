646a716a69e9ea58a372b1135fe453d281d14706
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 14:29:51 +0100

    hero

--- front-page.php
+++ front-page.php
@@ -1,4 +1,29 @@
 <?php get_header(); ?>
-<?php get_template_part( 'template-parts/flexible-content', 'none' ); ?>
-hello
+<?php
+
+// check if the flexible content field has rows of data
+if( have_rows('sections') ):
+
+     // loop through the rows of data
+    while ( have_rows('sections') ) : the_row();
+
+        if( get_row_layout() == 'hero' ):
+          the_sub_field('hero_title');
+        	get_template_part( 'partials/hero' );
+
+        elseif( get_row_layout() == 'portfolio' ):
+
+         get_template_part( 'partials/portfolio' );
+
+        endif;
+
+    endwhile;
+
+else :
+
+    // no layouts found
+
+endif;
+
+?>
 <?php get_footer(); ?>
