6b004124e5ba3e4eab260a087b5c1add06919019
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 11:50:21 +0100

    header

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
+  </head>
+  <body>
