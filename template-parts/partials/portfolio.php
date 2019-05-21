fd493fe6386a8256ab061969dbe23e89ba82bee5
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 16:35:35 +0100

    hero

--- template-parts/partials/portfolio.php
+++ template-parts/partials/portfolio.php
@@ -2,11 +2,11 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-6">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p><?php the_sub_field( 'text_content' ); ?></p>
       </div>
     </div>
     <div class="row">
-      <a href="#">Thsi is some text</a>
+      <a href="<?php the_sub_field( 'button_link' ); ?>"><?php the_sub_field( 'button_text' ); ?></a>
     </div>
   </div>
 </div>
