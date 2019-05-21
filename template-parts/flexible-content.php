9fcf7aec1a7531b5c290f2983597a7883f9314e9
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 13:43:27 +0100

    hero

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -6,13 +6,9 @@ if( have_rows('sections') ):
      // loop through the rows of data
     while ( have_rows('sections') ) : the_row();
 
-        if( get_row_layout() == 'hero' ): ?>
+        if( get_row_layout() == 'hero' ):
 
-        	Hello<?php get_template_part( 'hero', 'none' ); ?>
-
-      <?php  elseif( get_row_layout() == 'download' ):
-
-        	$file = get_sub_field('file');
+        	get_template_part( 'hero', 'none' );
 
         endif;
 
