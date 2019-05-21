6d60b93051e12b5f3cdd5a48f0bbdd7e1d3162a0
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 22:22:38 +0100

    hero

--- template-parts/partials/portfolio.php
+++ template-parts/partials/portfolio.php
@@ -1,6 +1,6 @@
 <div class="portfolio" style="background-image: url('<?php the_sub_field( 'background_image' ); ?>">
-  <div class="container-fluid fullHeight ">
-    <div class="row hangLeft">
+  <div class="container-fluid fullHeight fullCenterColumn">
+    <div class="row">
       <div class="col-md-6">
         <?php the_sub_field( 'text_content' ); ?>
         <a href="<?php the_sub_field( 'button_link' ); ?>"><?php the_sub_field( 'button_text' ); ?></a>
