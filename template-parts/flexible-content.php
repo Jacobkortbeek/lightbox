ee63ee27e4bb0acba8ff7122b6bfdb014000786d
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 14:24:01 +0100

    hero

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -2,12 +2,12 @@
 
 // check if the flexible content field has rows of data
 if( have_rows('sections') ):
-    
+
      // loop through the rows of data
     while ( have_rows('sections') ) : the_row();
 
         if( get_row_layout() == 'hero' ):
-
+          the_sub_field('hero_title');
         	get_template_part( 'partials/hero' );
 
         elseif( get_row_layout() == 'portfolio' ):
