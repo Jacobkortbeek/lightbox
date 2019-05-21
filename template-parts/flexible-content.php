41b2456fba45b0a4c7ce01778408ca3975dd9c37
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 13:40:27 +0100

    hero

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -1,27 +1,27 @@
 <?php
 
-// // check if the flexible content field has rows of data
-// if( have_rows('sections') ):
-//
-//      // loop through the rows of data
-//     while ( have_rows('sections') ) : the_row();
-//
-//         if( get_row_layout() == 'hero' ):
-//
-//         	the_sub_field('text');
-//
-//         elseif( get_row_layout() == 'download' ):
-//
-//         	$file = get_sub_field('file');
-//
-//         endif;
-//
-//     endwhile;
-//
-// else :
-//
-//     // no layouts found
-//
-// endif;
+// check if the flexible content field has rows of data
+if( have_rows('sections') ):
+
+     // loop through the rows of data
+    while ( have_rows('sections') ) : the_row();
+
+        if( get_row_layout() == 'hero' ):
+
+        	get_template_part( 'hero', 'none' );
+
+        elseif( get_row_layout() == 'download' ):
+
+        	$file = get_sub_field('file');
+
+        endif;
+
+    endwhile;
+
+else :
+
+    // no layouts found
+
+endif;
 
 ?>
