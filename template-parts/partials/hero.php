b1572753600abad1af9d5cd7550dcc5693efbf56
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 15:22:39 +0100

    hero

--- template-parts/partials/hero.php
+++ template-parts/partials/hero.php
@@ -5,8 +5,8 @@
         <h1><?php the_sub_field('hero_title'); ?></h1>
       </div>
     </div>
-    <div class="row">
-      <div class="hero-title fullCenter">
+    <div class="row fullCenter">
+      <div class="hero-title">
         <h3><?php the_sub_field('hero_subtitle'); ?></h3>
       </div>
     </div>
