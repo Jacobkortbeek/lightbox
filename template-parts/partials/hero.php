2884244b371e3962ebe0e4bd94714b8273703128
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 16:07:34 +0100

    hero

--- template-parts/partials/hero.php
+++ template-parts/partials/hero.php
@@ -6,7 +6,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="hero-title col-md-8 offset-md-3 mx-auto">
+      <div class="hero-subtitle col-md-8 offset-md-3 mx-auto">
         <h3><?php the_sub_field('hero_subtitle'); ?></h3>
       </div>
     </div>
