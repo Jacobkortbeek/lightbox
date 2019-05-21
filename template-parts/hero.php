b90288e5c9b85b662c37d34bcab383a83677225a
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 13:39:10 +0100

    hero

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,1 +1,9 @@
-Thsi is the hero
+<div class="hero">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="hero-title">
+        <?php the_sub_field('text'); ?>
+      </div>
+    </div>
+  </div>
+</div>
