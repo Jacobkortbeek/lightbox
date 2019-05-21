b460fa83e621e0738cfd16039874fa49ddf77b08
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 16:05:05 +0100

    hero

--- template-parts/partials/hero.php
+++ template-parts/partials/hero.php
@@ -11,18 +11,18 @@
       </div>
     </div>
     <div class="row">
-      <div class="hero-play-button col-md-8 offset-md-3 mx-auto">
+      <div class="hero-play-button col-md-8 offset-md-3 mx-auto pad-top-cont">
         <img src="<?php the_sub_field('play_button'); ?>" alt="play button">
 
       </div>
     </div>
     <div class="row">
-      <div class="hero-play-text col-md-8 offset-md-3 mx-auto">
+      <div class="hero-play-text col-md-8 offset-md-3 mx-auto pad-top-cont">
         <h3><?php the_sub_field('play_video_text'); ?></h3>
       </div>
     </div>
     <div class="row">
-      <div class="hero-title col-md-8 offset-md-3 mx-auto">
+      <div class="hero-title col-md-8 offset-md-3 mx-auto pad-top">
         <img src="<?php the_sub_field('down_arrow_image'); ?>" alt="down arrow">
       </div>
     </div>
