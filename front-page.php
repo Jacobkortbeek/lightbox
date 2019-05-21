0f64f6b8d856af7b6f18e84845dc3efbb7742992
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 14:02:26 +0100

    hero

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
-<?php get_template_part( 'template-parts/flexible-content', 'none' ); ?>
+<?php get_template_part( 'template-parts/partials/hero', 'none' ); ?>
 hello
 <?php get_footer(); ?>
