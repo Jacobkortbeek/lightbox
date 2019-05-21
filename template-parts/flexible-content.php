405453f02a1d59a14476ce45ce253886c450e8e6
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 14:36:01 +0100

    hero

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -6,15 +6,15 @@ if( have_rows('sections') ):
      // loop through the rows of data
     while ( have_rows('sections') ) : the_row();
 
-        if( get_row_layout() == 'hero' ):
-          the_sub_field('hero_title');
-        	get_template_part( 'partials/hero' );
+      if( get_row_layout() == 'hero' ):
+        the_sub_field('hero_title');
+      	get_template_part( 'partials/hero' );
 
-        elseif( get_row_layout() == 'portfolio' ):
+      elseif( get_row_layout() == 'portfolio' ):
 
-         get_template_part( 'partials/portfolio' );
+        get_template_part( 'partials/portfolio' );
 
-        endif;
+      endif;
 
     endwhile;
 
