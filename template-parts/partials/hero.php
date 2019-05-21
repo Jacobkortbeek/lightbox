117fa8e2aad8e7461521b0e7b451f7ddf0cfcb84
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 15:30:18 +0100

    hero

--- template-parts/partials/hero.php
+++ template-parts/partials/hero.php
@@ -1,11 +1,11 @@
 <div class="hero" style="background-image: url('<?php the_sub_field( 'background_image' ); ?>">
   <div class="container-fluid fullHeight fullCenterColumn" >
-    <div class="row ">
+    <div class="row mx-auto">
       <div class="hero-title col-md-6 offset-md-3">
         <h1><?php the_sub_field('hero_title'); ?></h1>
       </div>
     </div>
-    <div class="row">
+    <div class="row mx-auto">
       <div class="hero-title col-md-6 offset-md-3">
         <h3><?php the_sub_field('hero_subtitle'); ?></h3>
       </div>
