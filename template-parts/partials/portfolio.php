eba70fce613f71a232f6039196508b0e22ed5eed
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 17:07:20 +0100

    hero

--- template-parts/partials/portfolio.php
+++ template-parts/partials/portfolio.php
@@ -1,4 +1,4 @@
-<div class="portfolio">
+<div class="portfolio" style="background-image: url('<?php the_sub_field( 'background_image' ); ?>">
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-6">
