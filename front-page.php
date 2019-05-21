1dd56323480e981c0b66768ebcf5b4dcb46ba216
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-21 11:40:27 +0100

    header

--- front-page.php
+++ front-page.php
@@ -1,1 +1,13 @@
-Hello From the front page php
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      1
+    </div>
+    <div class="col-sm">
+      2
+    </div>
+    <div class="col-sm">
+      3
+    </div>
+  </div>
+</div>
--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php
 
 function wpt_theme_styles() {
+  wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
   wp_enqueue_style( 'styles', get_template_directory_uri() . '/css/styles.css' );
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+
+function wpt_theme_js() {
+  wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/css/bootstrap.min.js', array('jquery'), ' ', true );
+
+}
+add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
+
+?>
