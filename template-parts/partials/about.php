5574f353bbfa8153ca26ee6ade4af4513499b9c7
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 23:40:10 +0100

    hero

--- template-parts/partials/about.php
+++ template-parts/partials/about.php
@@ -9,7 +9,7 @@
           <div class="fontLg">
             <?php the_sub_field( 'founders_quote' ); ?>
           </div>
-          <div class="float-left">
+          <div class="float-right">
             <img class="float-right" src="<?php the_sub_field( 'close_quote_image' ); ?>" alt="">
           </div>
         <div class="prox fontSm">
