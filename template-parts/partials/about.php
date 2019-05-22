5d059105681077ca33f529c72fd3b232e8ecb117
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 23:29:32 +0100

    hero

--- template-parts/partials/about.php
+++ template-parts/partials/about.php
@@ -6,9 +6,10 @@
       </div>
       <div class="col-lg-4 offset-md-2 rightContent fullHeight fullCenterColumn">
         <img src="<?php the_sub_field( 'open_quote_image' ); ?>" alt="">
-        <div class="prox">
+
           <?php the_sub_field( 'founders_quote' ); ?>
         <img src="<?php the_sub_field( 'close_quote_image' ); ?>" alt="">
+        <div class="prox fontSm">
         <?php the_sub_field( 'quote_credit' ); ?>
         <?php the_sub_field( 'about_text' ); ?>
         <a href="<?php the_sub_field('button_link'); ?>"><?php the_sub_field('button_text'); ?></a>
