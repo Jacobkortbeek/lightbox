828d351fe55476104af91324927d1c032d2c4b26
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 21:46:15 +0100

    hero

--- functions.php
+++ functions.php
@@ -1,5 +1,16 @@
 <?php
 
+add_theme_support( 'menus' );
+
+function register_theme_menus() {
+  register_nav_menus(
+    array(
+      'primary-menu' => __( 'Primary Menu' )
+    )
+  );
+}
+add_action( 'init', 'register_theme_menus' );
+
 if( function_exists('acf_add_options_page') ) {
 
     acf_add_options_page(array(
