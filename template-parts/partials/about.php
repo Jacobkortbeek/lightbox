69e111c16d46a1ba8082286cf71e8432ac1b0826
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 10:59:57 +0100

    hero

--- template-parts/partials/about.php
+++ template-parts/partials/about.php
@@ -1,5 +1,5 @@
 <div class="about" style="background-image: url('<?php the_sub_field( 'background_image' ); ?>">
-  <div class="container-fluid fullHeight fullCenterLeft">
+  <div class="container-fluid fullHeight">
     <div class="row">
       <div class="col-lg-6 leftImg">
         <img src="<?php the_sub_field( 'founders_image' ); ?>" alt="Founders Images">
@@ -13,5 +13,5 @@
         <a href="<?php the_sub_field('button_link'); ?>"><?php the_sub_field('button_text'); ?></a>
       </div>
     </div>
-  </div> 
+  </div>
 </div>
