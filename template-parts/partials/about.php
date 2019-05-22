b0c7c9eb3d5bb550c39f8c33000c1503b7f8e581
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 10:37:22 +0100

    hero

--- template-parts/partials/about.php
+++ template-parts/partials/about.php
@@ -1,4 +1,4 @@
-<div class="portfolio" style="background-image: url('<?php the_sub_field( 'background_image' ); ?>">
+<div class="about" style="background-image: url('<?php the_sub_field( 'background_image' ); ?>">
   <div class="container-fluid fullHeight fullCenterLeft">
     <div class="row">
       <div class="col-md-6">
