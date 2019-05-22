061dac084f823bf4b570da5f0b938bc221903c69
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 11:21:00 +0100

    hero

--- template-parts/partials/about.php
+++ template-parts/partials/about.php
@@ -4,7 +4,7 @@
       <div class="col-lg-6 leftImg fullHeight">
         <img src="<?php the_sub_field( 'founders_image' ); ?>" alt="Founders Images">
       </div>
-      <div class="col-lg-4 offset-md-2 rightContent fullHeight">
+      <div class="col-lg-4 offset-md-2 rightContent fullHeight fullCenterColumn">
         <img src="<?php the_sub_field( 'open_quote_image' ); ?>" alt="">
         <?php the_sub_field( 'founders_quote' ); ?>
         <img src="<?php the_sub_field( 'close_quote_image' ); ?>" alt="">
