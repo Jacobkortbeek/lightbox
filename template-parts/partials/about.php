53dba147c00d60c11be142eabfa3d098a51865ff
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 23:45:25 +0100

    hero

--- template-parts/partials/about.php
+++ template-parts/partials/about.php
@@ -4,19 +4,27 @@
       <div class="col-lg-6 leftImg fullHeight">
         <img src="<?php the_sub_field( 'founders_image' ); ?>" alt="Founders Images">
       </div>
-      <div class="col-lg-4 offset-md-2 rightContent fullHeight fullCenterColumn">
-        <img class="float-left" src="<?php the_sub_field( 'open_quote_image' ); ?>" alt="">
-          <div class="fontLg">
-            <?php the_sub_field( 'founders_quote' ); ?>
-          </div>
-          <div class="float-right">
-            <img class="float-right" src="<?php the_sub_field( 'close_quote_image' ); ?>" alt="">
-          </div>
-        <div class="prox fontSm">
-        <?php the_sub_field( 'quote_credit' ); ?>
-        <?php the_sub_field( 'about_text' ); ?>
-        <a href="<?php the_sub_field('button_link'); ?>"><?php the_sub_field('button_text'); ?></a>
+      <div class="container">
+        <div class="row">
+          <img class="float-left" src="<?php the_sub_field( 'open_quote_image' ); ?>" alt="">
+            <div class="fontLg">
+              <?php the_sub_field( 'founders_quote' ); ?>
+            </div>
+            <div class="float-right">
+              <img class="float-right" src="<?php the_sub_field( 'close_quote_image' ); ?>" alt="">
+            </div>
         </div>
+        <div class="prox fontSm">
+        <div class="row">
+          <?php the_sub_field( 'quote_credit' ); ?>
+        </div>
+        <div class="row">
+          <?php the_sub_field( 'about_text' ); ?>
+        </div>
+        <div class="row">
+          <a href="<?php the_sub_field('button_link'); ?>"><?php the_sub_field('button_text'); ?></a>
+        </div>
+      </div>
       </div>
     </div>
   </div>
