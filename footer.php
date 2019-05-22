061e1e1cb18c025993ab4b07096ca4e5eb7c170a
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 11:54:57 +0100

    hero

--- footer.php
+++ footer.php
@@ -1,5 +1,25 @@
 <footer>
-  footer
+  <div class="container-fluid">
+    <div class="row">
+      <img src="" alt="">
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        content
+      </div>
+      <div class="col-md-4">
+        content
+      </div>
+      <div class="col-md-4">
+        content
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <a href="#" target="_blank"><img src="" alt=""></a>
+      </div>
+    </div>
+  </div>
 <?php wp_footer(); ?>
 </footer>
 </body>
