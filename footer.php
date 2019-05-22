e069cc2e9c152df3c68d786767057b1c6bf6029c
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 13:55:31 +0100

    hero

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <footer>
   <div class="container-fluid">
     <div class="row">
-      <img src="" alt="">
+      <img src="<?php the_field( 'logo', 'option' ); ?>" alt="">
     </div>
     <div class="row">
       <div class="col-md-4">
-        content | Content | Content
+        <?php the_field( 'office_address', 'option' ); ?> | <?php the_field( 'phone_number', 'option' ); ?> | <?php the_field( 'email_address', 'option' ); ?>
       </div>
     </div>
     <div class="row">
