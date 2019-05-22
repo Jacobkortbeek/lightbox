7c95f05e80968df1ea923ec58a5c4a3b13e81116
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-22 11:31:04 +0100

    hero

--- template-parts/partials/to-top.php
+++ template-parts/partials/to-top.php
@@ -1,6 +1,6 @@
 <div class="toTop">
   <div class="container">
-    <div class="row fullCenter">
+    <div class="row fullCenter padAll">
       <img src="<?php the_sub_field('to_top_image') ?>" alt="to top arrow">
     </div>
   </div>
