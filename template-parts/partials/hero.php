42ab42f32e2724a34e53420ab651d3fae9855165
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-23 01:09:16 +0100

    hero

--- template-parts/partials/hero.php
+++ template-parts/partials/hero.php
@@ -1,5 +1,5 @@
 <div class="hero" style="background-image: url('<?php the_sub_field( 'background_image' ); ?>">
-  <div class="container-fluid fullHeight fullCenterColumn" >
+  <div class="container-fluid fullHeight fullCenterColumnEven" >
     <div class="row">
       <div class="hero-title col-md-8 offset-md-3 mx-auto">
         <h1><?php the_sub_field('hero_title'); ?></h1>
@@ -11,18 +11,18 @@
       </div>
     </div>
     <div class="row">
-      <div class="hero-play-button col-md-8 offset-md-3 mx-auto fullCenter pad-top-cont">
+      <div class="hero-play-button col-md-8 offset-md-3 mx-auto fullCenter">
         <img src="<?php the_sub_field('play_button'); ?>" alt="play button">
 
       </div>
     </div>
     <div class="row">
-      <div class="hero-play-text col-md-8 offset-md-3 mx-auto pad-top-cont prox">
+      <div class="hero-play-text col-md-8 offset-md-3 mx-auto prox">
         <h3><?php the_sub_field('play_video_text'); ?></h3>
       </div>
     </div>
     <div class="row">
-      <div class="hero-title col-md-8 offset-md-3 mx-auto fullCenter pad-top">
+      <div class="hero-title col-md-8 offset-md-3 mx-auto fullCenter">
         <img src="<?php the_sub_field('down_arrow_image'); ?>" alt="down arrow">
       </div>
     </div>
