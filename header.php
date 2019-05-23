3e7ceae746b3e5385880f54a9a2183765b9773b6
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-23 01:02:36 +0100

    hero

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   </head>
   <body>
     <header>
-      <nav class="navbar navbar-expand-lg padAll">
+      <nav class="navbar navbar-expand-lg navbar-light padAll">
   <a class="navbar-brand" href="#"> <img src="<?php the_field('logo', 'option'); ?>" alt="logo"> </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
