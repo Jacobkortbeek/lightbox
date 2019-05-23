b5be3799364c03cfaaea50f76bd842a6943625c8
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-05-23 01:13:03 +0100

    hero

--- template-parts/partials/about.php
+++ template-parts/partials/about.php
@@ -1,12 +1,12 @@
 <div class="about" style="background-image: url('<?php the_sub_field( 'background_image' ); ?>">
   <div class="container-fluid fullHeight">
     <div class="row fullHeight">
-      <div class="col-lg-6 leftImg fullHeight">
+      <div class="col-lg-6 leftImg">
         <img class="float-left" src="<?php the_sub_field( 'founders_image' ); ?>" alt="Founders Images">
       </div>
-        <div class="col-lg-4 offset-md-2 rightContent fullHeight fullCenterColumn">
+        <div class="col-lg-4 offset-md-2 rightContent fullCenterColumn">
       <div class="container">
-        <div class="row padAll">
+        <div class="row">
           <img src="<?php the_sub_field( 'open_quote_image' ); ?>" alt="">
             <div class="fontLg">
               <p><?php the_sub_field( 'founders_quote' ); ?></p>
@@ -16,13 +16,13 @@
             </div>
         </div>
         <div class="prox fontSm">
-        <div class="row padAll">
+        <div class="row">
           <?php the_sub_field( 'quote_credit' ); ?>
         </div>
-        <div class="row padAll">
+        <div class="row">
           <?php the_sub_field( 'about_text' ); ?>
         </div>
-        <div class="row padAll">
+        <div class="row">
           <a href="<?php the_sub_field('button_link'); ?>"><?php the_sub_field('button_text'); ?></a>
         </div>
       </div>
